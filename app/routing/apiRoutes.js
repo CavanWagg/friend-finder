8a6d603ee60216d00d019413dbf6b4721eab8afc
Cavan Wagg <waggx002@gmail.com>
2018-05-12 07:15:13 -0500

    survey questions and form input capture

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,6 +23,10 @@ module.exports = function(app) {
 
 app.post("/api/friends", function(req, res) {
   res.json(true);
+  //. then(function(data)){
+    $('#id').text(data.name)
+  })
+  .catch();
   // 
 })
 }
